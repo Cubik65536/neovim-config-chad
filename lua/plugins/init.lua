dd2b8e90ffb50312e5b0d9cacde7c5c4517563a8
Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>
2025-01-28 01:13:32 -0500

    feat(plugins): Setup notification and cmdline
    Signed-off-by: Qian Qian "Cubik"‎ <cubik65536@cubik65536.top>

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,80 @@ return {
     end,
   },
 
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    config = function()
+      require("noice").setup({
+        cmdline = {
+            enabled = false, -- disables the Noice cmdline UI
+        },
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+        presets = {
+          bottom_search = false, -- use a classic bottom cmdline for search
+          command_palette = false, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false, -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      })
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    }
+  },
+
+  {
+    "gelguy/wilder.nvim",
+    lazy = false,
+    init = function ()
+      local wilder = require("wilder")
+
+      local gradient = {
+        "#4569ee", "#3987f9", "#2da3f3", "#26bce1", "#26d3c9",
+        "#2ee5ae", "#3ff393", "#57fb7a", "#74fe63", "#95fb51",
+        "#b5f242", "#d3e436", "#ecd12e", "#feb927" ,"#ff9f22",
+        "#ff821d", "#f96519", "#e54813", "#cb2f0d"
+      }
+
+      for i, fg in ipairs(gradient) do
+        gradient[i] = wilder.make_hl('WilderGradient' .. i, 'Pmenu', {{a = 1}, {a = 1}, {foreground = fg}})
+      end
+
+      wilder.setup({modes = {':', '/', '?'}})
+
+      wilder.set_option('renderer', wilder.popupmenu_renderer(
+        wilder.popupmenu_palette_theme({
+          -- 'single', 'double', 'rounded' or 'solid'
+          -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+          border = 'rounded',
+          max_height = '75%',      -- max height of the palette
+          min_height = 0,          -- set to the same as 'max_height' for a fixed height window
+          prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+          reverse = 0,             -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+
+          highlights = {
+            gradient = gradient, -- must be set
+            -- selected_gradient key can be set to apply gradient highlighting for the selected candidate.
+          },
+          highlighter = wilder.highlighter_with_gradient({
+            wilder.basic_highlighter(), -- or wilder.lua_fzy_highlighter(),
+          }),
+          left = {' ', wilder.popupmenu_devicons()},
+          right = {' ', wilder.popupmenu_scrollbar()},
+        })
+      ))
+    end
+  },
+
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
